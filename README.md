3cb24c253fd6639ef256e5833fb86fc13266e0eb
sohamb03 <sohamb03@outlook.com>
2021-01-05 16:01:00 +0100

    Xolentum Changes

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
-# monerod_exporter
-> Monero Prometheus exporter
+# Xolentumd Exporter
+> Xolentum Prometheus exporter
 
 ### Environment Variables
 
-* `DAEMON_HOST` - monerod host, default: `http://localhost:18081`
+* `DAEMON_HOST` - xolentumd host, default: `http://localhost:13580`
 * `PORT` - port to expose metrics on, default: `9396`
 
 ### Metrics Exported
 
-* `monerod_block_difficulty` - Last block difficulty
-* `monerod_block_reward` - Last block reward
-* `monerod_connections_incoming` - Number of incoming connections
-* `monerod_connections_outgoing` - Number of outgoing connections
-* `monerod_tx_chain` - Number of transactions in total
-* `monerod_tx_mempool` - Number of transactions in the mempool
+* `xolentumd_height` - Current block height
+* `xolentumd_block_difficulty` - Last block difficulty
+* `xolentumd_block_reward` - Last block reward
+* `xolentumd_connections_incoming` - Number of incoming connections
+* `xolentumd_connections_outgoing` - Number of outgoing connections
+* `xolentumd_connections_rpc` - Number of RPC connections
+* `xolentumd_tx_chain` - Number of transactions in total
+* `xolentumd_tx_mempool` - Number of transactions in the mempool
+* `xolentumd_database_size` - Current database size on disk
 
-### Pretty Screenshots
-
-You can use Grafana to create a dashboard to show everyone how cool you are.
-
-![Grafana Screenshot](https://i.imgur.com/2rqyOcY.png)
 
 ### License
 
-Released under the terms of the 3-Clause BSD License. See `LICENSE` for more
-information.
+[BSD 3-Clause License](LICENSE)
+
+Copyright © 2020 The Xolentum Project
+Copyright © 2018-2019 Excitable Aardvark 
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*
- * Copyright 2018 Excitable Aardvark <excitableaardvark@tutanota.de>
+ * Copyright 2020 The Xolentum Project <atom@xolentum.org>
  *
- * Licensed under the 3-Clause BSD license. See LICENSE in the project root for
+ * Licensed under the BSD 3-Clause License. See LICENSE in the project root for
  * more information.
  */
 
@@ -10,7 +10,7 @@ const express = require('express')
 const prometheus = require('prom-client')
 const util = require('util')
 
-const DAEMON_HOST = process.env.DAEMON_HOST || 'http://localhost:18081'
+const DAEMON_HOST = process.env.DAEMON_HOST || 'http://localhost:13580'
 const Gauge = prometheus.Gauge
 
 const app = express()
@@ -19,15 +19,15 @@ const daemon = new Daemon(DAEMON_HOST)
 let getInfo = util.promisify(daemon.getInfo.bind(daemon))
 let getLastBlockHeader = util.promisify(daemon.getLastBlockHeader.bind(daemon))
 
-const difficulty = new Gauge({ name: 'monerod_block_difficulty', help: 'Last block difficulty' })
-const incomingConnections = new Gauge({ name: 'monerod_connections_incoming', help: 'Number of incoming connections' })
-const mempoolSize = new Gauge({ name: 'monerod_tx_mempool', help: 'Number of transactions in the mempool' })
-const outgoingConnections = new Gauge({ name: 'monerod_connections_outgoing', help: 'Number of outgoing connections' })
-const reward = new Gauge({ name: 'monerod_block_reward', help: 'Last block reward' })
-const txCount = new Gauge({ name: 'monerod_tx_chain', help: 'Number of transactions in total' })
-const database_size = new Gauge({ name: 'monerod_database_size', help: 'Current database size on disk' })
-const height = new Gauge({ name: 'monerod_height', help: 'Current block height' })
-const rpc_connections_count = new Gauge({ name: 'monerod_rpc_connections_count', help: 'rpc_connections_count' })
+const difficulty = new Gauge({ name: 'xolentumd_block_difficulty', help: 'Last block difficulty' })
+const incomingConnections = new Gauge({ name: 'xolentumd_connections_incoming', help: 'Number of incoming connections' })
+const mempoolSize = new Gauge({ name: 'xolentumd_tx_mempool', help: 'Number of transactions in the mempool' })
+const outgoingConnections = new Gauge({ name: 'xolentumd_connections_outgoing', help: 'Number of outgoing connections' })
+const reward = new Gauge({ name: 'xolentumd_block_reward', help: 'Last block reward' })
+const txCount = new Gauge({ name: 'xolentumd_tx_chain', help: 'Number of transactions in total' })
+const database_size = new Gauge({ name: 'xolentumd_database_size', help: 'Current database size on disk' })
+const height = new Gauge({ name: 'xolentumd_height', help: 'Current block height' })
+const rpc_connections_count = new Gauge({ name: 'xolentumd_rpc_connections', help: 'rpc_connections_count' })
 
 app.get('/metrics', (req, res) => {
   Promise.all([
